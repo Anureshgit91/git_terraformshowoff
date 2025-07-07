e2c650f7dea57f8fe9524d27f42eb771b67cfd8f
Anureshgit91 <anuresh89@gmail.com>
2025-07-07 15:49:38 +0530

    terraform server datablock

--- Azurem_server_datablock/Main.tf
+++ Azurem_server_datablock/Main.tf
@@ -6,3 +6,14 @@
 # output "server_id" {
 #   value = data.azurerm_mssql_server.datasql.id
 # }
+
+
+ye line nicha wala add kar raha hai
+
+# data "azurerm_mssql_server" "datasql" {
+#   name                = var.name
+#   resource_group_name = var. 
+
+# output "server_id" {
+#   value = data.azurerm_m rver.datasql.id
+# }
